9ccb800952f59a85ea98dd4b74d739bdc1f74db2
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-01-22 16:31:29 -0400

    base pages

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const Router = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route />
+                <Route path="/" element={<SignIn />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </BrowserRouter>
     )
