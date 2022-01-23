66b338180569a1f9f85940930b57d5ec4b2db119
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-01-22 23:28:51 -0400

    signin page

--- frontend/src/pages/SignIn/styled.ts
+++ frontend/src/pages/SignIn/styled.ts
@@ -37,11 +37,13 @@ export const ButtonContainer = styled.div`
     flex-direction: column;
 
     p{
+        font-family: Roboto, sans-serif;
         font-size: 0.75rem;
         font-weight: 400;
         color: ${({theme}) => theme.colors.secondary};
 
         a{
+            font-family: Roboto, sans-serif;
             font-size: 1rem;
             font-weight: 700;
         }
