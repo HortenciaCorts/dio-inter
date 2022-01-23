4d89be384e6251cbd4aacc334fc7dae94d90e765
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-01-22 20:08:11 -0400

    card component

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,10 +1,14 @@
 import { Wrapper, Background, InputContainer, ButtonContainer } from "./styled";
 import background from '../../assets/images/background-login.jpg';
+import Card from "../../components/Card";
 
 const SignIn = () => {
     return(
         <Wrapper>
             <Background image={background} />
+            <Card width="483px">
+                Olá
+            </Card>
         </Wrapper>
     );
 }
