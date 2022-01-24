5c254b3f85bcb0c203757b2e11594cd63e93fe65
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-01-24 13:54:04 -0400

    adding cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,15 @@ import { createConnection } from 'typeorm';
 import routes from './routes';
 import { globalErrors } from './middlewares/globalErros';
 
+import cors from "cors"
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 createConnection().then(connection => {
   const app = express();
+  app.use(cors(corsOptions)) // Use this after the variable declaration
   const PORT = 3333;
   
   app.use(express.json())
