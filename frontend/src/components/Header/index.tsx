84573f17bbd6b71644cbfc15316fa15e84e691f7
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-01-24 14:00:57 -0400

    interacting the back with the front

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,17 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import {HeaderContainer, HeaderWrapper, UserInfo} from './styles';
 
-import UserCircle from '../UserCircle';
-// import useAuth from '../../hooks/useAuth';
-
+import useAuth from '../../hooks/useAuth';
 import logoInter from '../../assets/images/Inter-orange.png';
-import { useNavigate } from 'react-router-dom';
+import UserCircle from '../UserCircle';
 
 const Header = () => {
    
     const navigate = useNavigate();
-    // const {user} = useAuth();
+    const {user} = useAuth();
 
-    // const initials = user.firstName.substring(0,1) + user.lastName.substring(0,1);
+    const initials = user.firstName.substring(0,1) + user.lastName.substring(0,1);
 
     const handleLogoff = () => {
         navigate('/signin');
@@ -21,10 +20,10 @@ const Header = () => {
             <HeaderWrapper>
                    <img src={logoInter} width={172} height={61} alt="logo inter" />
                 <UserInfo>
-                  {/* <UserCircle initials={initials} /> */}
+                  <UserCircle initials={initials} />
                   <div>
-                      {/* <p>Olá, <span className="primary-color font-bold">{user.firstName}</span></p> */}
-                      {/* <strong>{user.accountNumber}-{user.accountDigit}</strong><br/> */}
+                      <p>Olá, <span className="primary-color font-bold">{user.firstName} {user.lastName}</span></p>
+                      <strong>{user.accountNumber}-{user.accountDigit}</strong><br/>
                       <a href="/" onClick={handleLogoff}>Sair</a>
                   </div>
                 </UserInfo>
